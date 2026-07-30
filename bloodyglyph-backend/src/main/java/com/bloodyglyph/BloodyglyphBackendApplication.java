a785a928d1647bba6dfb5782dfd498c0952fbd34
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-07-30 18:06:00 +0200

    backend setup

---
+++ bloodyglyph-backend/src/main/java/com/bloodyglyph/BloodyglyphBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bloodyglyph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodyglyphBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodyglyphBackendApplication.class, args);
+    }
+
+}
---
+++ bloodyglyph-backend/src/test/java/com/bloodyglyph/BloodyglyphBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodyglyph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodyglyphBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
