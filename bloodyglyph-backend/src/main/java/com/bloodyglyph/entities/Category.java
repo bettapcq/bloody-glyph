98a737236b37a48fba0cd1a2df98b0804c954ff4
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-07-30 19:49:38 +0200

    create category entity

---
+++ bloodyglyph-backend/src/main/java/com/bloodyglyph/entities/Category.java
@@ -0,0 +1,29 @@
+package com.bloodyglyph.entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+}
