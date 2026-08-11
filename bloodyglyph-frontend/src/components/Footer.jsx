7c6efbefc8307258cca1cc342343e59269dc3a07
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-08-12 00:19:19 +0200

    update footer

--- bloodyglyph-frontend/src/components/Footer.jsx
+++ bloodyglyph-frontend/src/components/Footer.jsx
@@ -2,7 +2,15 @@ function Footer() {
   return (
     <footer className="w-full grid gap-4 lg:grid-cols-[1fr_2fr_1fr] lg:gap-8 bg-[var(--color-surface)]/60 backdrop-blur-sm py-4">
       {" "}
+<<<<<<< Updated upstream
       <div className="flex flex max-w-7xl  items-center justify-center md:justify-start gap-1 lg:px-4 lg:py-4 text-[var(--color-text-secondary)]">
+=======
+<<<<<<< Updated upstream
+      <div className="flex flex max-w-7xl  items-center justify-center md:justify-start gap-1 px-4 py-4 text-[var(--color-text-secondary)]">
+=======
+      <div className="flex flex max-w-7xl  items-center justify-center lg:justify-start gap-1 lg:px-4 lg:py-4 text-[var(--color-text-secondary)]">
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
         <img src="/logo-circle.png" alt="Logo" className="h-16 w-auto" />
       </div>
       <div className="mx-auto flex flex-col max-w-7xl  items-center justify-center gap-1 lg:px-4 lg:py-4 text-[var(--color-text-secondary)]">
@@ -26,8 +34,6 @@ function Footer() {
           <a
             href="/cookie-policy"
             target="_blank"
-            Cookie
-            Policy
             rel="noopener noreferrer"
             className="text-[var(--color-primary)] hover:text-[var(--color-primary-hover)]"
           >
@@ -38,8 +44,6 @@ function Footer() {
           <a
             href="/privacy-policy"
             target="_blank"
-            Privacy
-            Policy
             rel="noopener noreferrer"
             className="text-[var(--color-primary)] hover:text-[var(--color-primary-hover)]"
           >
