aa4b0dc3c91a4da15b884f06d2a4dcce8a5d9ffe
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-08-22 07:25:34 +0200

    add dashboard section

---
+++ bloodyglyph-frontend/src/components/DashboardSection.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <> </>;
+}
+
+export const DashboardSection;
---
+++ bloodyglyph-frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+function DashboardPage() {
+  return (
+    <main className="relative min-h-screen">
+      <Navbar />
+      <DashboardSection />
+      <Footer />
+    </main>
+  );
+}
+
+export const DashboardPage;
